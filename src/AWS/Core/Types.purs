01f5ad49e2b282baa5008b80cb8a10361e6384cc
tiphanie <tiphanie.dousset@hivemindtechnologies.com>
2020-11-18 08:08:53 +0000

    Resolve #169 : Add external types and create mapping toExternal

--- src/AWS/Core/Types.purs
+++ src/AWS/Core/Types.purs
@@ -1,9 +1,10 @@
 module AWS.Core.Types where
 
-import Prelude
+import Prelude (class Show)
 import Data.Newtype (class Newtype)
-import Simple.JSON (class WriteForeign)
+import Simple.JSON (class WriteForeign, class ReadForeign)
 import Data.Maybe (Maybe)
+import Data.DateTime (DateTime)
 
 newtype AccessKeyId
   = AccessKeyId String
@@ -60,13 +61,73 @@ derive instance ntInstanceType :: Newtype InstanceType _
 
 derive newtype instance showInstanceType :: Show InstanceType
 
-type BasicClientPropsR r 
+type BasicClientPropsR r
   = ( accessKeyId :: Maybe AccessKeyId
     , secretAccessKey :: Maybe SecretAccessKey
     , region :: Maybe Region
     , sessionToken :: Maybe SessionToken
-    | r 
+    | r
     )
 
-type DefaultClientPropsR = BasicClientPropsR ()
-type DefaultClientProps = Record DefaultClientPropsR
+type DefaultClientPropsR
+  = BasicClientPropsR ()
+
+type DefaultClientProps
+  = Record DefaultClientPropsR
+
+newtype NextPageToken
+  = NextPageToken String
+
+derive instance ntNextPageToken :: Newtype NextPageToken _
+
+derive newtype instance showNextPageToken :: Show NextPageToken
+
+derive newtype instance wfNextPageToken :: WriteForeign NextPageToken
+
+derive newtype instance rfNextPageToken :: ReadForeign NextPageToken
+
+newtype Key
+  = Key String
+
+derive instance ntKey :: Newtype Key _
+
+derive newtype instance showKey :: Show Key
+
+derive newtype instance wfKey :: WriteForeign Key
+
+derive newtype instance rfKey :: ReadForeign Key
+
+newtype Amount
+  = Amount String
+
+derive instance ntAmount :: Newtype Amount _
+
+derive newtype instance showAmount :: Show Amount
+
+derive newtype instance wfAmount :: WriteForeign Amount
+
+derive newtype instance rfAmount :: ReadForeign Amount
+
+type GroupDefinition
+  = { key :: Maybe Key }
+
+type DateInterval
+  = { start :: DateTime, end :: DateTime }
+
+type MetricValue
+  = { amount :: Maybe Amount }
+
+type Metric
+  = { unblendedCost :: Maybe MetricValue }
+
+type Group
+  = { keys :: Maybe (Array Key), metrics :: Maybe Metric }
+
+type ResultByTime
+  = { timePeriod :: Maybe DateInterval, groups :: Maybe (Array Group) }
+
+type CostAndUsage
+  = { resultsByTime :: Maybe (Array ResultByTime)
+    , groupDefinitions :: Maybe (Array GroupDefinition)
+    , nextPageToken :: Maybe NextPageToken
+    }
